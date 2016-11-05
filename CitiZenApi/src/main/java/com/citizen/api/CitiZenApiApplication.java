01b1b75772b404034ff11a48d589d09b272ac6fb
tykco <purepker0@gmail.com>
2016-11-05 13:36:18 +0800

    Pre finals first commit github

--- CitiZenApi/src/main/java/com/citizen/api/CitiZenApiApplication.java
+++ CitiZenApi/src/main/java/com/citizen/api/CitiZenApiApplication.java
@@ -20,27 +20,31 @@ public class CitiZenApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(CitiZenApiApplication.class, args);
         
-//     		CitiAPI api = new CitiAPI();
-//     		
-//     		//To authorize
-//     		api.authorize();
-//     		System.out.println("Token Type: " + api.getTokenType());
-//     		System.out.println("Access Token: " + api.getAccessToken());
-//     		
-//     		//To pay with points
-//     		//1st enroll credit card(lastFourDigitsCardNumber, citiCardHolderPhoneNumber, merchantCustomerReferenceId)
-//     		api.enrolCreditCard("7411", "2608191234", "2608191234111");
-//     		//2nd activate point payment (linkageConfirmationCode)
-//     		api.activatePointPayment("735937");
-//     		//3rd retrieve point balance
-//     		api.retrievePointBalance();
-//     		System.out.println("Currency Code: " + api.getCurrencyCode());
-//     		System.out.println("Program Conversion Rate: " + api.getProgramConversionRate());
-//     		System.out.println("AvailablePointBalance: " + api.getAvailablePointBalance());
-//     		System.out.println("Maximum Points To Reedem: " + api.getMaximumPointsToReedem());
-//     		System.out.println("Minimum Points To Reedem: " + api.getMinimumPointsToReedem());
-//     		//4th pay with points (transactionReferenceNumber, currencyCode, pointsToRedeem, transactionDescription)
-//     		api.payWithPoints("132323454de6234543", "SGD", 2000, "My Test Transaction");
+     		CitiAPI api = new CitiAPI();
+     		
+     		//To authorize
+     		api.authorize();
+     		System.out.println("Token Type: " + api.getTokenType());
+     		System.out.println("Access Token: " + api.getAccessToken());
+     		
+     		//To pay with points
+     		//1st enroll credit card(lastFourDigitsCardNumber, citiCardHolderPhoneNumber, merchantCustomerReferenceId)
+     		System.out.println("Enrolling CreditCard....");
+     		api.enrolCreditCard("7411", "2608191234", "2608191234111");
+     		//2nd activate point payment (linkageConfirmationCode)
+     		System.out.println("Activating Point Payment....");
+     		api.activatePointPayment("735937");
+     		//3rd retrieve point balance
+     		System.out.println("Retrieving Balance");
+     		api.retrievePointBalance();
+     		System.out.println("Currency Code: " + api.getCurrencyCode());
+     		System.out.println("Program Conversion Rate: " + api.getProgramConversionRate());
+     		System.out.println("AvailablePointBalance: " + api.getAvailablePointBalance());
+     		System.out.println("Maximum Points To Reedem: " + api.getMaximumPointsToReedem());
+     		System.out.println("Minimum Points To Reedem: " + api.getMinimumPointsToReedem());
+     		//4th pay with points (transactionReferenceNumber, currencyCode, pointsToRedeem, transactionDescription)
+     		System.out.println("Paying with Points....");
+     		api.payWithPoints("132323454de6234543", "SGD", 2000, "My Test Transaction");
      		
     }
     
